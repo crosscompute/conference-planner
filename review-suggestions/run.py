7a76cb04ee541ad8a0d5beaada8d96b22753e9d0
Roy Hyunjin Han <rhh@crosscompute.com>
2023-05-23 14:55:09 -0400

    Render table in review-suggestions

--- review-suggestions/run.py
+++ review-suggestions/run.py
@@ -8,17 +8,28 @@ output_folder = Path(getenv(
     'CROSSCOMPUTE_OUTPUT_FOLDER', 'batches/standard/output'))
 datasets_folder = Path('datasets')
 runs_folder = datasets_folder / 'runs'
+
+
+columns = [
+    'website_url',
+    'subject',
+    'start_date',
+    'end_date'
+    'location',
+    'talk_deadline',
+    'proposal_url']
+rows = []
+for run_folder in runs_folder.glob('*'):
+    with (run_folder / 'input' / 'variables.dictionary').open('rt') as f:
+        d = json.load(f)
+    rows.append(d.values())
+
+
+with (output_folder / 'suggestions.json').open('wt') as f:
+    json.dump({'columns': columns, 'data': rows}, f)
+
+
 with (output_folder / 'suggestions.csv').open('wt') as f:
     csv_writer = csv.writer(f)
-    csv_writer.writerow([
-        'website_url',
-        'subject',
-        'start_date',
-        'end_date'
-        'location',
-        'talk_deadline',
-        'proposal_url'])
-    for run_folder in runs_folder.glob('*'):
-        with (run_folder / 'input' / 'variables.dictionary').open('rt') as f:
-            d = json.load(f)
-        csv_writer.writerow(d.values())
+    csv_writer.writerow(columns)
+    csv_writer.writerows(rows)
